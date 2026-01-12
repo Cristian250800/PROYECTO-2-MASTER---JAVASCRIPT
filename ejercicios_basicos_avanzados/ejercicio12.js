697d6dba6cb2a491de376c6377795fa76729998d
Cristian250800 <cristianmancha15@gmail.com>
2026-01-12 19:28:52 +0100

    Ejercicio 8,9,10,11,12,13,14,15 juntos

---
+++ ejercicios_basicos_avanzados/ejercicio12.js
@@ -0,0 +1,30 @@
+/*Valores únicos: Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, 
+en caso que existan los elimina para retornar un array sin los elementos duplicados.
+Pista: puedes generar un nuevo array y devolverlo.
+Puedes usar este array para probar tu función:*/
+
+const duplicates = [
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+];
+
+function removeDuplicates(list) {
+    const unico = [];
+    for (const i of list) {
+        if(!unico.includes(i)){
+            unico.push(i)
+        }
+    }
+    console.log(unico)
+}
+
+removeDuplicates(duplicates);
---
+++ ejercicios_basicos_avanzados/ejercicio13.js
@@ -0,0 +1,34 @@
+/*Buscador de nombres: Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array. 
+Esta función comprueba si existe el elemento, y en caso de que exista 
+nos devuelve un true y la posición de dicho elemento y por la contra un false.
+Puedes usar este array para probar tu función:*/
+
+const names = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+
+let buscar = "Clint"
+
+function nameFinder(nameList, valor) {
+  if (nameList.includes(valor)){
+    return {
+        encontrado: true,
+        posicion: nameList.indexOf(valor)
+    } ;
+  } 
+  else {
+    return false
+  }
+}
+
+console.log(nameFinder(names, buscar))
